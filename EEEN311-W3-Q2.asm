44b871823b9202acfd86a4d12be57c0841da0cdf
Ugurturkel <83291622+Ugurmutturkel@users.noreply.github.com>
2022-12-26 09:59:38 +0300

    Update EEEN311-W3-Q2.asm

--- EEEN311-W3-Q2.asm
+++ EEEN311-W3-Q2.asm
@@ -4,12 +4,13 @@ org 100h
     ;mov al,arr[0] ;al will be 10h
     ;si : the register index for arrays    
     
-    mov si,5h
+    mov si,4h
     mov ax,0
     sum:
     add al,arr[si]
     dec si
-    jnz sum 
+    jnz sum  
+    add al,arr[0h]
 
 ;takes the sum of a 5 length array with loop
 
